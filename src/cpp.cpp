be54604b43c65afb68372579c87c82cbcd37c6d1
Filip Lobpreis <f.lobpreis@gmail.com>
2022-04-13 13:47:26 +0200

    CMakeLists.txt update
    asdf

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -2,7 +2,7 @@
 
 void cppCreation(const std::string& dir, bool qt){
 	std::string header = "Header.h", source = "Source.cpp";
-	std::string command = "mkdir " + dir + " && cd " + dir + " && touch main.cpp " + header + " " + source +" build.sh compile.sh CMakeLists.txt && mkdir cmake-build && chmod +x build.sh compile.sh";
+	std::string command = "mkdir " + dir + " && cd " + dir + " && touch main.cpp " + header + " " + source +" build.sh compile.sh CMakeLists.txt && chmod +x build.sh compile.sh";
 	int trash = system(command.c_str());
 
 	command = dir + "/main.cpp";
@@ -27,12 +27,15 @@ void cppCreation(const std::string& dir, bool qt){
 			<< "project(" << dir << " VERSION 0.1.0)\n\n"
 
 			<< "set(CMAKE_CXX_STANDARD 17)\n"
-			<< "set(CMAKE_CXX_STANDARD_REQUIRED ON)\n\n"
+			<< "set(CMAKE_CXX_STANDARD_REQUIRED ON)\n"
+			<< "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n\n"
 
 			<< "set(CMAKE_AUTOMOC ON)\n"
 			<< "set(CMAKE_AUTORCC ON)\n"
 			<< "set(CMAKE_AUTOUIC ON)\n"
-			<< "set(Qt5_DIR \"/usr/lib/x86_64-linux-gnu/cmake/Qt5/Qt5Config.cmake\")\n\n"
+			<< "set(Qt5_DIR \"/usr/lib/x86_64-linux-gnu/cmake/Qt5/Qt5Config.cmake\")\n"
+			<< "set(SOURCES\n\tmain.cpp\n\tSource.cpp\n)\n\n"
+			<< "set(HEADERS\n\tHeader.h\n)\n\n"
 
 			<< "if(CMAKE_VERSION VERSION_LESS \"3.7.0\")\n"
 				<< "\tset(CMAKE_INCLUDE_CURRENT_DIR ON)\n"
@@ -40,31 +43,35 @@ void cppCreation(const std::string& dir, bool qt){
 
 			<< "find_package(Qt5 COMPONENTS Widgets REQUIRED)\n\n"
 
-			<< "add_executable(" << dir << "\n"
-				<< "\tmain.cpp Source.cpp\n"
-			<< ")\n"
-			<< "target_link_libraries(" << dir <<" PUBLIC Qt5::Core)\n" << std::endl;
+			<< "add_executable(${PROJECT_NAME}\n"
+				<< "\t${SOURCES}\n"
+				<< "\t${HEADERS}\n"
+			<< ")\n\n"
+			<< "target_link_libraries(${PROJECT_NAME} PUBLIC Qt5::Core)\n" << std::endl;
 	} else {
 		file << "cmake_minimum_required(VERSION 3.0.0)\n"
 			<< "project(" << dir << " VERSION 0.1.0)\n\n"
 			<< "set(CMAKE_CXX_STANDARD 17)\n"
 			<< "set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n\n"
-			<< "add_executable(" << dir << " main.cpp Source.cpp)\n"
-			<< "#target_link_libraries(" << dir << " pthread)\n#example of linking libraries";
+			<< "set(SOURCES\n\tmain.cpp\n\tSource.cpp\n)"
+			<< "add_executable(${PROJECT_NAME}\n"
+				<< "\t${SOURCES}\n"
+			<< ")\n"
+			<< "#target_link_libraries(${PROJECT_NAME} pthread)\n#example of linking libraries" << std::endl;
 	}
 	file.close();
 
 	command = dir + "/build.sh";
 	file.open(command, std::ios::out);
-	file << "#!/bin/sh\ncmake --build ./cmake-build\nif [ $? -eq 0 ];then\n./cmake-build/" << dir << "\nfi";
+	file << "#!/bin/sh\ncmake --build ./build\nif [ $? -eq 0 ];then\n./build/" << dir << "\nfi";
 	file.close();
 
 	command = dir + "/compile.sh";
 	file.open(command, std::ios::out);
-	file << "#!/bin/sh\n\nclear\ncmake --build ./cmake-build\n";
+	file << "#!/bin/sh\n\nclear\ncmake --build ./build\n";
 	file.close();
 
-	command = "cd " + dir + " && cmake CMakeLists.txt -S . -B ./cmake-build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 && mv ./cmake-build/compile_commands.json .";
+	command = "cd " + dir + " && cmake CMakeLists.txt -S . -B ./build && mv ./build/compile_commands.json .";
 	trash = system(command.c_str());
 }
 
