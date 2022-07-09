450633bd3ae132b63616da67e0d61c3bb109c844
Filip Lobpreis <f.lobpreis@gmail.com>
2022-07-09 22:33:13 +0200

    core: language: Default language initialization to none
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/filetypes/java/java.cpp
+++ src/filetypes/java/java.cpp
@@ -13,7 +13,7 @@ void makeJavaProject(const std::string &dir, const flags &languageFlags)
 	generateBuildFiles(dir, slashedPackedLocation, packageLocation, languageFlags.maven);
 
 	if (languageFlags.maven) {
-		command = "mkdir -p " + dir + "/src/main/java && mv " + dir + "/" + slashedPackedLocation.substr(0, slashedPackedLocation.find_first_of("/"))
+		command = "mkdir -p " + dir + "/src/main/java && mkdir -p /src/test/java && mv " + dir + "/" + slashedPackedLocation.substr(0, slashedPackedLocation.find_first_of("/"))
 				+ " " + dir + "/src/main/java && chmod 666 " + dir + "/src/main/java/" + slashedPackedLocation + "/Main.java";
 
 		executeCommand(command);
