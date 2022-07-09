b91417bae68485256ea73e63055c5f7fe96f04b4
Filip Lobpreis <f.lobpreis@gmail.com>
2022-07-09 22:33:13 +0200

    core: language: Default language initialization to none
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/core/flags.h
+++ src/core/flags.h
@@ -12,6 +12,7 @@ struct flags
 	{
 		java,
 		cpp,
+		none,
 	};
 
 	language lang;
@@ -36,7 +37,7 @@ struct flags
 	 * @brief Constructor
 	 */
 	flags()
-	: lang()
+	: lang(language::none)
 	, qt(false)
 	, openCV(false)
 	, maven(false)
