1197f8a04958388a8c491bd9d2e7a404945b732d
Filip Lobpreis <f.lobpreis@gmail.com>
2022-09-25 13:54:57 +0200

    init: Initialize the project update
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- init.sh
+++ init.sh
@@ -2,18 +2,30 @@ cmake -S . -B ./build
 cd build/
 nice make -j4 && cp ./buildProject ..
 
-cd ../startup/
-DIR=$(pwd)
+cd ..
+BUILD_DIR="$(pwd)/build"
+STARTUP_DIR="$(pwd)/startup"
 LINK=$HOME/.local/share/zinit/completions/_buildProject
 
 if [[ $SHELL == "/usr/bin/zsh" || $SHELL == "/bin/zsh" ]]; then
-	echo "export PATH="$PATH:$DIR/build"" >> $HOME/.zshrc
+	echo "export PATH=\"$PATH:$BUILD_DIR\"" >> $HOME/.zshrc
 	if [[ ! -f $LINK ]]; then
-		ln -s $DIR/startup/_buildProject.zsh $LINK
+		ln -s $STARTUP_DIR/_buildProject.zsh $LINK
 	fi
 elif [[ $SHELL == "/bin/bash" ]]; then
-	echo "export PATH="$PATH:$DIR/build"" >> $HOME/.bashrc
-	echo "source "$DIR./init.bash"" >> $HOME/.bashrc
+	echo "export PATH=\"\$PATH:$BUILD_DIR\"" >> $HOME/.bashrc
+	echo "source $STARTUP_DIR/init.bash" >> $HOME/.bashrc
+	. ~/.bashrc
+fi
+
+read -p "Do you want to generate documentation? [Y/n]: " i
+if [[ $i == "n" ]]; then
+	exit 0
+fi
+
+which doxygen
+if [[ $? != 0  ]]; then
+	sudo apt install doxygen -y
 fi
 
 cd ../doc
